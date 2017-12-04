84b60d97bae723f233793a5ce22d32f449826c11
charliewang <31499655+mrwtakev@users.noreply.github.com>
2017-12-04 09:29:41 +0800

    点云txt格式转换pcd格式

---
+++ build/CMakeFiles/txt_to_pcd.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/charlie/CLionProjects/txt to pcd/main.cpp" "/Users/charlie/CLionProjects/txt to pcd/build/CMakeFiles/txt_to_pcd.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "//usr/local/Cellar/pcl/1.8.1_1"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/txt_to_pcd.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/txt_to_pcd.dir/main.cpp.o"
+  "txt_to_pcd.pdb"
+  "txt_to_pcd"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/txt_to_pcd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
